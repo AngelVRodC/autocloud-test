4d2d880e83c467995b3dcf78b6fb13d5fb152828
Angel Rodriguez <arodriguez2552@gmail.com>
2021-05-26 22:40:25 -0500

    auth middleware with custom token

---
+++ src/middlewares/index.ts
@@ -0,0 +1,42 @@
+
+import dotenv from 'dotenv';
+import passport from 'passport';
+import { NextFunction, Request, Response } from 'express';
+import { UniqueTokenStrategy } from 'passport-unique-token';
+
+dotenv.config();
+
+const strategyOptions = {
+  tokenHeader: 'custom-token',
+  failOnMissing: true
+};
+
+passport.use(
+  new UniqueTokenStrategy(strategyOptions, (token: string, done) => {
+    const { CUSTOM_TOKEN } = process.env;
+    if (token === CUSTOM_TOKEN) {
+      return done(null, {});
+    }
+    return done({ name: 'UNAUTHORIZED', message: 'Incorrect token credentials'});
+  }),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user: any, done) => {
+  done(null, user);
+});
+
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  passport.authenticate('token', 
+    (error, user, x) => {
+      if (error || !user) {
+        res.status(401).send(error);
+      } else {
+        next();
+      }
+  })(req, res, next);
+}
