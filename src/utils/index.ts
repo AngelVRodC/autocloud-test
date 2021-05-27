7df4d55cf47df591021fed833b017b8e8af0d1b6
Angel Rodriguez <arodriguez2552@gmail.com>
2021-05-26 22:40:48 -0500

    utils functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,43 @@
-export const snakeToCamelCase = (str: string) =>
+export const snakeCaseToCamelCase = (str: string) =>
   str.replace(/([-_]\w)/g, g => g[1].toUpperCase());
+
+export const objectKeysInSnakeCaseToCamelCase = (input: any) => {
+  let newObject = {};
+  Object.keys(input).forEach((key: string) => {
+    newObject = {
+      ...newObject,
+      [snakeCaseToCamelCase(key)]: input[key]
+    };
+  });
+  return newObject;
+}
+
+export const deleteNullAttributes = (input: any) => {
+  const output = { ...input };
+  Object.keys(output).forEach((key: string) => {
+    if (output[key] === null) {
+      delete output[key];
+    }
+  });
+  
+  return output;
+};
+
+export const groupArrayByAttribute = (array: any[], attribute: string) => {
+  return array.reduce((rv, x) => {
+    (rv[x[attribute]] = rv[x[attribute]] || []).push(x);
+    return rv;
+  }, {});
+};
+
+
+export const sortArrayByAttribute = (array: any[], attribute: string) => {
+  return array.sort((a, b) => {
+    if (a[attribute] > b[attribute])
+      return 1;
+    if (a[attribute] < b[attribute])
+      return -1;
+    return 0;
+  });
+}
+
